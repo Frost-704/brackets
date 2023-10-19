07258fe4984babdff0c62f3f1b70e8afeeb67fd4
Frost-704 <boolean.pg.me@gmail.com>
2023-10-19 23:11:03 +0300

    finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let testArray = str.split('');
+  let start = [];
+  let end = [];
+  let arrLength = str.length;
+  let stack = [];
+  for (let test of bracketsConfig) {
+    start.push(test[0]);
+    end.push(test[1]);
+  }
+    for (let i = 0; i < arrLength; i += 1) {
+      if (end.includes(testArray[i]) && stack.length !== 0 && `${stack[stack.length - 1]}${end[start.indexOf(stack[stack.length - 1])]}` === `${stack[stack.length - 1]}${testArray[i]}`) {
+        stack.pop();
+        } else {
+        stack.push(testArray[i]);
+        } 
+      }
+  return stack.length === 0 ?  true : false;
 }
+
